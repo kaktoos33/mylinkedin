53f980a7a73ae72590ef0684f20dfe75586af3fc
Mehdi Anoosheh <med.anooshe@gmail.com>
2022-04-12 18:52:05 +0430

    first commit

---
+++ src/main/java/ir/manoosheh/mylinkedin/MyLinkedinApplication.java
@@ -0,0 +1,13 @@
+package ir.manoosheh.mylinkedin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLinkedinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLinkedinApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ir/manoosheh/mylinkedin/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ir.manoosheh.mylinkedin;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MyLinkedinApplication.class);
+	}
+
+}
